53cbdd8db2ca7be6a222c9b1b007dab8c1838733
Yu Jinyan <yujinyan@outlook.com>
2021-01-16 15:38:31 +0800

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,20 @@ mod method_area;
 #[allow(non_camel_case_types)]
 #[derive(Debug)]
 enum Opcode {
-    // push int const 0
     iload_0,
     iconst_0,
     iconst_1,
     iload_1,
     istore_0,
     istore_1,
-    // store into local variable 1
     iadd,
     ireturn,
     goto(usize),
-    // goto
     iinc(usize, i32),
-    // increment local variable
     bipush(i32),
-    // push int constant
     if_icmplt(usize),
-    //
     r#return,
-    invokestatic(usize)
+    invokestatic(usize),
 }
 
 
